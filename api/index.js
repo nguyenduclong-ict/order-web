383d9ab826c99bec88f4040fa5dcb8e8f0ac8670
Nguyen Duc Long <ndl23061997@gmail.com>
2019-03-28 11:06:07 +0700

    up

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ router.use('/register', require('./register'));
 router.use('/provider', auth.authProvider, require('./provider/test'));
 router.use('/upload', require('./upload'));
 // Router admin
-// router.use('/admin/*', auth.authAdmin);
+router.use('/admin/*', auth.authAdmin);
 router.use('/admin/user',  require('./admin/user'));
 router.use('/admin/category', require('./admin/category'));
 module.exports = router;
