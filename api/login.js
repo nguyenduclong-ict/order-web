bb3ec448b94b2cd3bfccd1b00dbc8e30d16f3489
Nguyen Duc Long <ndl23061997@gmail.com>
2019-04-25 23:53:45 +0700

    asd

--- api/login.js
+++ api/login.js
@@ -58,7 +58,7 @@ async function postLogin (req ,res) {
                             message: 'Đăng nhập thành công',
                             token: token,
                             imageCode : r._id,
-                            role : result.type 
+                            role : s
                         })
                     } catch (error) {
                         return res.status(200).json({
