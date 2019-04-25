c732135c4cfab4a520cca36871f42f5f8d1f35a6
Nguyen Duc Long <ndl23061997@gmail.com>
2019-04-25 21:59:21 +0700

    asd

--- helpers/MyMongoose.js
+++ helpers/MyMongoose.js
@@ -5,6 +5,7 @@ var db_url = process.env.DATABASE_URL;
 // Connect mongoose
 mongoose.connect(db_url, {useNewUrlParser: true}, (err, succ) => {
     if(!err) console.log('database connect success');
+    else console.log('Connect database failure','\n', err);
 });
 
 var Types = {};
